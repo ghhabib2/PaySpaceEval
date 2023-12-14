6beeabe70071df81442f1aefb164100613ef8497
ghhabib <ghaffh1@mcmaster.ca>
2023-12-14 15:11:25 -0500

    feat: Adding the serlizers for Wallet and transactions

---
+++ src/serializers/blockchain_serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+
+
+class TransAction(serializers.ModelSerializer):
+    """
+    Transaction serializer class
+    """
+    class Meta:
+        # model = TransAction # TODO: Will work on it later
+        fields = '__all__'
+
+    tx_hash = serializers.CharField(help_text="Transaction hash")
+    block_height = serializers.IntegerField(help_text="Block Height")
+    tx_input_n = serializers.IntegerField(help_text="Transaction input number")
+    tx_output_n = serializers.IntegerField(help_text="Transaction output number")
+    value = serializers.IntegerField(help_text="value")
+    ref_balance = serializers.IntegerField(help_text="Ref balance")
+    spent = serializers.BooleanField(help_text="Spent flag")
+    confirmations = serializers.IntegerField(help_text="Confirmations")
+    confirmed = serializers.DateTimeField("Confirmation Date and Time")
+    double_spend = serializers.BooleanField(help_text="Double Spend flag")
+
+class Wallet(serializers.ModelSerializer):
+    """
+    Wallet serializer class
+    """
+    class Meta:
+        # model = Wallet # TODO: Will work on it later
+        fields = '__all__'
+
+    address = serializers.CharField(help_text="Wallet Address")
+    total_received = serializers.IntegerField(help_text="Total amount received in the Wallet")
+    total_sent = serializers.IntegerField(help_text="Total sent by the wallet")
+    balance = serializers.IntegerField(help_text="Wallet Balance")
+    unconfirmed_balance = serializers.IntegerField(help_text="Nnconfirmed Balance")
+    final_balance = serializers.IntegerField(help_text="Final Balace")
+    n_tx = serializers.IntegerField(help_text="Number of transactions")
+    unconfirmed_n_tx = serializers.IntegerField(help_text="Number of unconfirmed transactions")
+    final_n_tx = serializers.IntegerField(help_text="Final transaction")
+    tx_url = serializers.CharField(help_text="Transaction Url")
+    txrefs = TransAction(many=True, read_only=True)
+
+
+
--- src/utils/blockchain.py
+++ src/utils/blockchain.py
@@ -11,14 +11,26 @@ class BlockChain(object):
         self.__address_end_point = \
             f"{settings.BlockCypher_BASE_ADDRESS}{settings.BlockCypher_COIN}/{settings.BlockCypher_CHAIN}/{settings.BlockCypher_Wallet_Address}"
         """
-        internal variable generating the wallet address
+        internal variable to work the wallet
         """
         self.__transaction_end_point = \
             f"{settings.BlockCypher_BASE_ADDRESS}{settings.BlockCypher_COIN}/{settings.BlockCypher_CHAIN}/{settings.BlockCypher_Transaction_Address}"
+        """
+        internal variable to work on transactions
+        """
+
 
     def generate_wallet(self):
         """
-        This will generate a wallet
+        generate a new address and return the following information:
+            - The Address.
+            - The public key.
+            - The secret key.
 
-        :return:
+        :return: Return this tuple (Address,Public Key,Secret Key)
+        :rtype: (str,str,str)
         """
+
+        raise NotImplemented("This method has not been yet.")
+
+    def check_wallet_status(self):
